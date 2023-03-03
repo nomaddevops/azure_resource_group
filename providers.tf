0422ab5cc21371727da314626a74eb0a236f72db
Joffrey Dupire <joffrey.dupirewefight.co>
2023-03-03 15:55:26 +0100

    deps: update terraform version/ azurerm version, and update lockfile

--- providers.tf
+++ providers.tf
@@ -1,17 +1,11 @@
 
-###############################################################
-#                          AZURE_RG                           #
-# Purpose: Deploy an Azure resource gorup and his components  #
-#                   Last Updated: 01/11/22                    #
-###############################################################
 terraform {
-  required_version = ">= 1.1.3"
-  experiments      = [module_variable_optional_attrs]
+  required_version = "= 1.3.9"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.45.0"
+      version = "= 3.46.0"
     }
   }
 
